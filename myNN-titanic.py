191cd9d4931db0eb35ab45b8655514541be02b7d
windelu <2551553092@qq.com>
2016-08-01 14:27:26 +0800

    change new code, but this is a binary classfier problem, i need change the net

--- myNN-titanic.py
+++ myNN-titanic.py
@@ -3,27 +3,6 @@ import numpy as np
 import pandas as pa
 
 
-
-'''
-1.survival   *     Survival
-                      (0 = No; 1 = Yes)
-2.pclass     *     Passenger Class
-                     (1 = 1st; 2 = 2nd; 3 = 3rd)
-3.name   delete    Name
-4.sex      (0,1)   Sex
-5.age       *      Age
-6.sibsp     *      Number of Siblings/Spouses Aboard
-7.parch     *      Number of Parents/Children Aboard
-8.ticket  delete   Ticket Number
-9.fare    delete   Passenger Fare
-10.cabin  delete   Cabin
-11.emb    delete   Port of Embarkation
-                (C = Cherbourg; Q = Queenstown; S = Southampton)
-'''
-
-
-
-
 def predata(datapath,isTrain):
     '''
     delete unused number and change sex:male:0  female:1
@@ -32,7 +11,6 @@ def predata(datapath,isTrain):
     :return:
     '''
     data = pa.read_csv(datapath).values
-    # data2=[]
     if isTrain == False:
         data2 = np.delete(data, [0, 2, 7, 8, 9, 10], axis=1)
         for i in data2:
@@ -81,8 +59,13 @@ def myNN(train_path,test_path,net,batch_size,epochs,learningrate):
     Return type:	mxnet.Executor
     '''
     exe = net.simple_bind(ctx=mx.cpu(), **input_info)
+    exe_info = dict(zip(net.list_arguments(), exe.arg_arrays))
 
     #initial weight and bias
+    init = mx.init.Uniform(scale=0.08)
+    for i, j in exe_info.items():
+        if i not in input_info:
+            init(i, j)
     #combile network arguments and simple_bind allocate array
     exe_info = dict(zip(net.list_arguments(), exe.arg_arrays))
     # print exe_info
@@ -91,10 +74,10 @@ def myNN(train_path,test_path,net,batch_size,epochs,learningrate):
     #
     # label = exe_info[train_iter.provide_label[0][0]]
     # print data,label
-    init = mx.init.Uniform(scale=0.08)
-    for i,j in exe_info.items():
-        if i not in input_info:
-            init(i,j)
+    # init = mx.init.Uniform(scale=0.08)
+    # for i,j in exe_info.items():
+    #     if i not in input_info:
+    #         init(i,j)
 
     #update
     updater = update_SGD(learningrate,train_iter)
@@ -110,9 +93,10 @@ def myNN(train_path,test_path,net,batch_size,epochs,learningrate):
             # label[:] = batch.label[0]
             exe_info['data'][:]=batch.data[0]     #[:] is very important but why
             # print 'batch.data shape', batch.data.__len__()  # batch.data shape 1
-            # print 'batch.data[0]',batch.data[0]
+            # print 'batch.data[0]',batch.data[0].asnumpy()
             # print 'exe_info[\'data\']',exe_info['data']
             exe_info['softmax_label'][:]= batch.label[0]
+            # print 'batch labe',batch.label[0].asnumpy()
             # print 'batch label ',batch.label[0].asnumpy()
             exe.forward(is_train=True)
             exe.backward()
@@ -120,16 +104,31 @@ def myNN(train_path,test_path,net,batch_size,epochs,learningrate):
             for index, args in enumerate(zip(exe.arg_arrays,exe.grad_arrays)):
                 weight, grad = args
                 updater(index,grad,weight)#update each weight and grad
+
             metric.update(batch.label,exe.outputs)
+
+            # pred_prob = exe.outputs[0].asnumpy()
+            # pred = np.argmax(pred_prob, axis=1)
+            # print pred
+            # print 'output :', exe.outputs[0].asnumpy()
+            # print 'compare label:', batch.label[0].asnumpy()
             num += 1
             if num % 20 == 0:
                 print 'epoch',epoch,'iter',num,'metric',metric.get()
         # out = []
         # for catch in test_iter:
-        #     exe_info['data'][:] = catch.data[0]
-        #     exe.forward()
-        #     print exe.outputs[0].asnumpy()
-        #     # test_output.append(out)
+        #     # print 'test data', catch.data[0].asnumpy()
+        #     exe_info['data'][:] =catch.data[0]
+        #     exe.forward(is_train=False)
+        #     print 'output :',exe.outputs[0].asnumpy()
+        # # exe_info['data'][:] = mx.nd.array(test_data)
+        # # exe.forward()
+
+
+
+
+
+
     # return test_output
 
 
@@ -143,22 +142,22 @@ def loaddata2DataIter(train_path,batchsize):
 
     train_d, train_l= predata(train_path,True)
     train_iter = mx.io.NDArrayIter(train_d, train_l, batch_size=batchsize)
-    # for batch in train_iter:
-    #     data = batch.data[0]
-    #     print type(data)
     return train_iter
+
 
 
 def update_SGD(learnigrate,train_iter):
     opt_SGD = mx.optimizer.SGD(
         learning_rate=learnigrate,
         momentum=0.1,
-        wd=0.00001,
+        wd=0.001,
         rescale_grad=1.0/(train_iter.batch_size )#True or False
     )
     update = mx.optimizer.get_updater(opt_SGD)
     return update
     # return opt_SGD
+
+
 def createNetWorkL1(hidden):
     '''
        create a one layer network,
@@ -170,7 +169,7 @@ def createNetWorkL1(hidden):
     # output_layer = mx.sym.Variable('out')
     # layer1: fullyconnected
     fc = mx.sym.FullyConnected(data=imput_layer, name='fullconnectlayer', num_hidden=hidden)
-    act = mx.sym.Activation(data=fc, name='actlayer', act_type='relu')
+    act = mx.sym.Activation(data=fc, name='actlayer', act_type='tanh')
     net = mx.sym.SoftmaxOutput(data=act, name='softmax')
     return net
 
@@ -190,10 +189,9 @@ def createNetWorkL3(h1,h2,h3):
     fc2 = mx.sym.FullyConnected(data=act1, name='fc2', num_hidden=h2)
     act2 = mx.sym.Activation(data=fc2, name='act2', act_type='relu')
     fc3 = mx.sym.FullyConnected(data=act2, name='fc3', num_hidden=h3)
-    act3 = mx.sym.Activation(data=fc3, name='act3', act_type='relu')
+    act3 = mx.sym.Activation(data=fc3, name='act3', act_type='sigmoid')
     net = mx.sym.SoftmaxOutput(data=act3, name='softmax')
     return net
-
 
 
 
@@ -203,29 +201,28 @@ if __name__ == '__main__':
     test_path = "/home/xuqian/mxnet2/data/titinic_test.csv"
     # t = predata(test_path,False)
     # print t.shape
-    batch_size = 40
-    hidden = 10
+    batch_size = 8
+    hidden = 5
     h1 = 10
-    h2 = 10
-    h3 = 1
-    epoch = 20
-    learningrate = 0.001
-    net = createNetWorkL3(h1=h1,
+    h2 = 50
+    h3 = 10
+    epoch = 10
+    learningrate = 2
+    net1 = createNetWorkL1(hidden=hidden)
+    net3 = createNetWorkL3(h1=h1,
                           h2=h2,
                           h3=h3
                           )
-    # epoch  8  test('accuracy', 0.9996666666666667)
-    # epoch  9  test('accuracy', 1.0)
     myNN(
         train_path=train_path,
         test_path=test_path,
-        net=net,
+        net=net3,
         batch_size=batch_size,
         epochs=epoch,
         learningrate=learningrate
     )
-    # for i in out:
-    #     print i
+
+
 
 
 
